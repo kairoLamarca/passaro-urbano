616832b4dbf46517da33350d0c018f0c7f660962
kairolamarca <lamarca.kairo@gmail.com>
2017-11-15 11:06:21 -0200

    incluindo components e assets
    inclusão dos components home, topo, rodape e assets de css e imagens

---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-topo',
+  templateUrl: './topo.component.html',
+  styleUrls: ['./topo.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class TopoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
