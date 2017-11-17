419c55f4573abded2ef1657be424118619a9cd19
kairolamarca <lamarca.kairo@gmail.com>
2017-11-17 00:00:29 -0200

    exemplo promise encadeada
    exemplo promise encadeada, uma promise dentro da outra

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -80,7 +80,13 @@ export class OfertasService {
         .then((ofertas: Oferta[]) =>{
             //faz outra tratativa
             console.log('segundo then');
+            return new Promise((resolve2, reject2) =>{
+                setTimeout(() => {resolve2(ofertas)}, 3000)
+            });
+        })
+        .then(( ofertas: Oferta[]) =>{
+            console.log('terceiro then executado após 3 segundos porque estava aguardando uma promise ser resolvida');
             return ofertas;
-        });
+        })
     }
 }
