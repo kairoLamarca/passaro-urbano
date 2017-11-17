425a08e1053d018267f7a59aa336055a80e67b3e
kairolamarca <lamarca.kairo@gmail.com>
2017-11-16 22:35:04 -0200

    ofertas.service
    Desenvolvido classe ofertas.serve e referenciado ela na home utilizando provider

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) { }//instanciando por provider
 
   ngOnInit() {
+    console.log(this.ofertasService.getOfertas());
   }
 
 }
