6a53842d43df3feba1bd0ea940245f70292c61db
kairolamarca <lamarca.kairo@gmail.com>
2017-11-20 01:29:05 -0200

    components diversao e restaurante
    components diversao e restaurante

---
+++ src/app/diversao/diversao.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-diversao',
+  templateUrl: './diversao.component.html',
+  styleUrls: ['./diversao.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class DiversaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/restaurantes/restaurantes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurantes',
+  templateUrl: './restaurantes.component.html',
+  styleUrls: ['./restaurantes.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class RestaurantesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
