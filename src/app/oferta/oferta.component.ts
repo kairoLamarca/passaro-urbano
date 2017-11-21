1a970cf4b3167aaa7f86ed505a44c47de7e9413a
kairolamarca <lamarca.kairo@gmail.com>
2017-11-21 21:53:25 -0200

    inclsão da tela de oferta e configuração de rota
    inclusão da tela de oferta que receberá o id da oferta como parametro e configuração da rota com o ID

---
+++ src/app/oferta/oferta.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-oferta',
+  templateUrl: './oferta.component.html',
+  styleUrls: ['./oferta.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class OfertaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
