9993d111916bbf99dbbe31ecd137578d99888baf
kairolamarca <lamarca.kairo@gmail.com>
2017-11-22 23:15:34 -0200

    components comoUsar, OndeFica e router children
    Criação dos componentes ComoUsar e OndeFica e configuração das rotas filhas dentro do arquivo routes referenciando dentro de ofertas

---
+++ src/app/oferta/como-usar/como-usar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-como-usar',
+  templateUrl: './como-usar.component.html',
+  styleUrls: ['./como-usar.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ComoUsarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/oferta/onde-fica/onde-fica.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-onde-fica',
+  templateUrl: './onde-fica.component.html',
+  styleUrls: ['./onde-fica.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class OndeFicaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
