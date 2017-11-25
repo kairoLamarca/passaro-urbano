9cded58cb0fa9f968585746b3b0bda14289d717c
kairolamarca <lamarca.kairo@gmail.com>
2017-11-25 17:48:25 -0200

    rota filha acessando parametro da rota pai e metodos do onde fica e como usar
    rota filha acessando parametro da rota pai utilizando o parent do snapshot e metodos do onde fica e como usar acessando dados da api

--- src/app/oferta/como-usar/como-usar.component.ts
+++ src/app/oferta/como-usar/como-usar.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OfertasService } from '../../ofertas.service';
 
 @Component({
   selector: 'app-como-usar',
   templateUrl: './como-usar.component.html',
   styleUrls: ['./como-usar.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  providers: [OfertasService]
 })
 export class ComoUsarComponent implements OnInit {
 
-  constructor() { }
+  public comoUsar: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService
+  ) { }
 
   ngOnInit() {
+    //console.log('ID da rota pai: ', this.route.parent.snapshot.params['id']);//acessar parametros da rota pai
+    this.ofertasService.getComoUsarOfertaPorId(this.route.parent.snapshot.params['id'])
+      .then((descricao: string) => {
+        this.comoUsar = descricao;
+      });
   }
 
 }
