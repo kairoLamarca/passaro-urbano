8ad6738172635a6c067e9eff13d85b2daa38385c
Kairo Lamarca <lamarca.kairo@gmail.com>
2017-11-30 23:31:07 -0200

    inicio de tratativa com observable e subject
    inicio de tratativa com observable e subject

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
 import { Observable } from 'rxjs/Observable';
 import { Oferta } from '../shared/oferta.model';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/switchMap'/
 
 @Component({
   selector: 'app-topo',
@@ -13,10 +15,13 @@ import { Oferta } from '../shared/oferta.model';
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>;
+  private subjectPesquisa: Subject<string> = new Subject<string>();
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa //retorno Oferta[]
+      .switchMap();
   }
 
   // public pesquisa(event: Event): void{
@@ -24,12 +29,14 @@ export class TopoComponent implements OnInit {
   // }
 
   public pesquisa(termoDaBusca: string): void {
-    this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca);
-    this.ofertas.subscribe(
-      (ofertas: Oferta[]) => console.log(ofertas),
-      (erro: any) => console.log('Erro status: ', erro.status),//segundo parametro é o erro
-      () => console.log('Fluxo de eventos completo')//terceiro paraemtro é a conclusão
-    )
+    // this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca);
+    // this.ofertas.subscribe(
+    //   (ofertas: Oferta[]) => console.log(ofertas),
+    //   (erro: any) => console.log('Erro status: ', erro.status),//segundo parametro é o erro
+    //   () => console.log('Fluxo de eventos completo')//terceiro paraemtro é a conclusão
+    // )
+
+    this.subjectPesquisa.next(termoDaBusca)
   }
 
 }
