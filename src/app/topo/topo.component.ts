2e725550c9beef46a87fdbd46c54396deebe6272
Kairo Lamarca <lamarca.kairo@gmail.com>
2017-12-04 23:12:08 -0200

    refactoring da importação do rxjs
    refactoring da importação do rxjs, crianção um .ts somente com as importações

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -3,12 +3,8 @@ import { OfertasService } from '../ofertas.service';
 import { Observable } from 'rxjs/Observable';
 import { Oferta } from '../shared/oferta.model';
 import { Subject } from 'rxjs/Subject';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/operator/catch';
 
+import '../util/rxjs-extensions';
 
 @Component({
   selector: 'app-topo',
