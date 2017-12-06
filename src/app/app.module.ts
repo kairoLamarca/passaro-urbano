5bf871dc4d83460c89d57abe132ba68f4cb4517d
Kairo Lamarca <lamarca.kairo@gmail.com>
2017-12-05 23:19:29 -0200

    utilização do LOCALE_ID para mudar a região para mostrar virgula nos valores monetarios
    inicialmente utilizando o intl, estava dando erro, assim usei o locales do próprio angular e funcionou

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -14,6 +14,10 @@ import { DiversaoComponent } from './diversao/diversao.component';
 import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
+
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [
@@ -32,7 +36,7 @@ import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
     HttpModule,
     RouterModule.forRoot(ROUTES)//mapeamento de rotas global pra aplicação
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'pt-Br'} ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
