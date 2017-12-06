bd73b2b439e0d3eef571c6b590426c133179abeb
Kairo Lamarca <lamarca.kairo@gmail.com>
2017-12-05 22:38:31 -0200

    exemplo de pipe de date
    exemplo de pipe de date

--- src/app/restaurantes/restaurantes.component.ts
+++ src/app/restaurantes/restaurantes.component.ts
@@ -11,7 +11,9 @@ import { OfertasService } from '../ofertas.service';
 })
 export class RestaurantesComponent implements OnInit {
 
-  public ofertas: Oferta[]
+  public ofertas: Oferta[];
+
+  // public dataTeste: any = new Date(2017, 8, 30);
 
   constructor(private ofertasService: OfertasService) { }
 
