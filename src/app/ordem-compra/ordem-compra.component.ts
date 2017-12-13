f42073882618f9297b57878c85080b93e5ecc2e0
kairoLamarca <lamarca.kairo@gmail.com>
2017-12-13 14:13:48 -0200

    populando a model pedido
    populando a model pedido e enviando ela para a service

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service';
+import { Pedido } from '../shared/pedido.model';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -9,6 +10,9 @@ import { OrdemCompraService } from '../ordem-compra.service';
   providers: [OrdemCompraService]
 })
 export class OrdemCompraComponent implements OnInit {
+
+  //Pedido
+  public pedido: Pedido = new Pedido('', '', '', '');
 
   public endereco: string = '';
   public numero: string = '';
@@ -104,5 +108,14 @@ export class OrdemCompraComponent implements OnInit {
     else {
       this.formEstado = 'disabled';
     }
+  }
+
+  public confirmarCompra(): void {
+    this.pedido.endereco = this.endereco;
+    this.pedido.numero = this.numero;
+    this.pedido.complemento = this.complemento;
+    this.pedido.formaPagamento = this.formaPagamento;
+
+    this.ordemCompraService.efetivarCompra(this.pedido);
   }
 }
