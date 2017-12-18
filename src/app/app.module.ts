83888b8ac9e0e729d1a3c59127b67bf33729874d
kairoLamarca <lamarca.kairo@gmail.com>
2017-12-18 13:44:25 -0200

    importação do FormsModule para template forms
    importação do FormsModule para template forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { ROUTES } from './app.routes';
 
@@ -42,6 +43,7 @@ import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,//Recurso de template forms
     RouterModule.forRoot(ROUTES)//mapeamento de rotas global pra aplicação
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'pt-Br' }],
