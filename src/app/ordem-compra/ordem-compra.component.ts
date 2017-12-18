44969ae20d7ff8b373644b2aedc86c1701e26d73
kairoLamarca <lamarca.kairo@gmail.com>
2017-12-18 17:05:31 -0200

    validação para mostrar tela de pedido inserido
    validação para mostrar tela de pedido inserido

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -14,6 +14,8 @@ export class OrdemCompraComponent implements OnInit {
   //permite decorar um atributo da classe com os valores contidos em uma variavel de referencia do template
   @ViewChild('formulario') public f: NgForm;
 
+  public idPedidoCompra: number;
+
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
@@ -31,7 +33,7 @@ export class OrdemCompraComponent implements OnInit {
 
     this.ordemCompraService.efetivarCompra(pedido)
       .subscribe((idPedido: number) =>{
-        console.log('Pedido cadastrado com sucesso! ID do Pedido: ' + idPedido);
+        this.idPedidoCompra = idPedido;
       });
   }
 }
