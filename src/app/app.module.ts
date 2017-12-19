d5578bff5b5e5be4f9528564d6aad94f1134ad9b
kairoLamarca <lamarca.kairo@gmail.com>
2017-12-19 14:13:03 -0200

    restauração do component ordem compra para utilizar reactive form
    restauração do component ordem compra para utilizar reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+//import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ROUTES } from './app.routes';
 
@@ -43,7 +44,8 @@ import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule,//Recurso de template forms
+    //FormsModule,//Recurso de template forms
+    ReactiveFormsModule,
     RouterModule.forRoot(ROUTES)//mapeamento de rotas global pra aplicação
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'pt-Br' }],
--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
 
@@ -7,33 +6,16 @@ import { Pedido } from '../shared/pedido.model'
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
   styleUrls: ['./ordem-compra.component.css'],
-  providers: [OrdemCompraService]
+  providers: [ OrdemCompraService ]
 })
 export class OrdemCompraComponent implements OnInit {
-
-  //permite decorar um atributo da classe com os valores contidos em uma variavel de referencia do template
-  @ViewChild('formulario') public f: NgForm;
-
-  public idPedidoCompra: number;
 
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
-
+    
   }
 
   public confirmarCompra(): void {
-
-    let pedido: Pedido = new Pedido(
-      this.f.value.endereco,
-      this.f.value.numero,
-      this.f.value.complemento,
-      this.f.value.formaPagamento
-    );
-
-    this.ordemCompraService.efetivarCompra(pedido)
-      .subscribe((idPedido: number) =>{
-        this.idPedidoCompra = idPedido;
-      });
   }
 }
