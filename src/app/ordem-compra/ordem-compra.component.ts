5c6cb0bfa006c94decc99343250a3d70661f9234
kairoLamarca <lamarca.kairo@gmail.com>
2017-12-21 14:17:59 -0200

    implementação do botão confirma compra e chamada da tela de sucesso
    implementação do botão confirma compra e chamada da tela de sucesso passando um parametro por @input

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -11,13 +11,14 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public idPedidoCompra: number;
   // FormGroup = formulario
   // FormControls = elementos do formulaeio
 
   //formulario é sincronizado com o form no html
   public formulario: FormGroup = new FormGroup({
     'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
-    'numero': new FormControl(null, [ Validators.required, Validators.minLength(1), Validators.maxLength(20)]),
+    'numero': new FormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(20)]),
     'complemento': new FormControl(null),
     'formaPagamento': new FormControl(null, [Validators.required])
   });
@@ -29,11 +30,24 @@ export class OrdemCompraComponent implements OnInit {
   }
 
   public confirmarCompra(): void {
-    if (this.formulario.status === 'INVALID'){
+    if (this.formulario.status === 'INVALID') {
       this.formulario.get('endereco').markAsTouched();
       this.formulario.get('numero').markAsTouched();
       this.formulario.get('complemento').markAsTouched();
       this.formulario.get('formaPagamento').markAsTouched();
+    }
+    else {
+      let pedido: Pedido = new Pedido(
+        this.formulario.value.endereco,
+        this.formulario.value.numero,
+        this.formulario.value.complemento,
+        this.formulario.value.formaPagamento
+      );
+
+      this.ordemCompraService.efetivarCompra(pedido)
+        .subscribe((idPedido: number) => {
+          this.idPedidoCompra = idPedido;
+        });
     }
   }
 }
